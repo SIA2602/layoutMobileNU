d20d9dd0e810deed8d7515636e394ee733bda2ed
Sia2602 <danilojose26@hotmail.com>
2021-05-29 16:09:30 -0300

    Adicionando Recurso da Tela Inicial.

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+
+function ocultDisplay(){    
+    document.getElementById("entrada").style.display = "none";
+    document.getElementById("menu").style.display = "block";
+}
+
+window.setTimeout(ocultDisplay, 1000);
+
