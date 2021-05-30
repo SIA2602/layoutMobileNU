5d57a8b6453a0f3a20d2db8846e3c5666294f78b
Sia2602 <danilojose26@hotmail.com>
2021-05-30 14:58:35 -0300

    Adicionando textos no slider vertical.

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,28 @@ function ocultDisplay(){
 }
 
 window.setTimeout(ocultDisplay, 1500);
+
+var cont = true;
+function oculta(){    
+    if(!cont){
+        document.getElementById("saldoFatura01").style.display = "block";
+        document.getElementById("saldoFatura02").style.display = "block";
+        document.getElementById("saldo").style.display = "block";
+        document.getElementById("textoOculto01").style.display = "none";   
+        document.getElementById("textoOculto02").style.display = "none";     
+        
+        var img = document.getElementById("imageEyes");
+        img.src="assets/img/eyes.png";
+    }
+    else{
+        document.getElementById("saldoFatura01").style.display = "none";
+        document.getElementById("saldoFatura02").style.display = "none";
+        document.getElementById("saldo").style.display = "none";
+        document.getElementById("textoOculto01").style.display = "block"; 
+        document.getElementById("textoOculto02").style.display = "block"; 
+
+        var img = document.getElementById("imageEyes");
+        img.src="assets/img/noEyes.png";
+    }   
+    cont = !cont; 
+}
